f6833f8c9813162f31447960a3ff0bfc2fc28053
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-11-17 18:22:25 -0300

    feat: parse complete mailbox files
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- src/parser/mailbox.rs
+++ src/parser/mailbox.rs
@@ -1,1 +1,39 @@
+use message::Message;
+
+use crate::utils;
+
 pub mod message;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Mailbox<'input> {
+    pub messages: Vec<Message<'input>>,
+}
+
+impl<'input> TryFrom<&'input str> for Mailbox<'input> {
+    type Error = String;
+
+    fn try_from(input: &'input str) -> Result<Self, Self::Error> {
+        let messages = utils::capture_messages(input)
+            .iter()
+            .map(|message| Message::try_from(*message))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Mailbox { messages })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_mailbox() {
+        let input = include_str!("mailbox/samples/multi_patches.mbx");
+        let mailbox = Mailbox::try_from(input);
+        assert!(mailbox.is_ok());
+        let mailbox = mailbox.unwrap();
+
+        dbg!(&mailbox);
+        assert!(mailbox.messages.len() == 3);
+    }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,66 +1,30 @@
-/// Returns the next line in the input string
-pub fn next_line(input: &str) -> Option<&str> {
-    input.find('\n').map(|i| &input[i + 1..])
-}
-
-/// Returns the next non-empty line in the input string
-pub fn next_non_empty_line(input: &str) -> Option<&str> {
+fn find_messages(input: &str) -> Vec<usize> {
     let mut input = input;
-    while let Some(line) = next_line(input) {
-        if line.trim().is_empty() {
-            input = line;
-        } else {
-            return Some(line);
-        }
+    let mut messages = Vec::new();
+    let mut start = 0;
+
+    while let Some(next) = input.find("\nFrom ") {
+        let next = next + 1;
+        messages.push(start + next);
+        input = &input[next..];
+        start += next;
     }
-    None
+    
+    messages
 }
 
-/// Given a mailbox file, returns the next message in the file
-/// 
-/// It will start looking from the `start` index in the input string
-/// for the message delimiter pattern `--\n<number>\n\nFrom `. and 
-/// return the index of the start of the message (the index of `F`).
-pub fn find_next_message(input: &str, start: usize) -> Option<usize> {
-    let input = &input[start..];
-    let mut lines = input.lines();
-    let mut stage = 0;
-    let mut index = start;
+pub fn capture_messages(input: &str) -> Vec<&str> {
+    let mut messages = Vec::new();
+    let mut start = 0;
 
-    while let Some(line) = lines.next() {
-        match stage {
-            0 => {
-                if line.trim_end() == "--" {
-                    stage = 1;
-                }
-            },
-            1 => {
-                if line.chars().all(|c| c.is_digit(10) || c == '.') {
-                    stage = 2;
-                } else {
-                    stage = 0;
-                }
-            },
-            2 => {
-                if line.is_empty() {
-                    stage = 3;
-                } else {
-                    stage = 0;
-                }
-            },
-            3 => {
-                if !line.starts_with("From ") && !line.trim().is_empty() {
-                    stage = 0;
-                } else if line.starts_with("From ") {
-                    return Some(index);
-                }
-            },
-            _ => unreachable!()
-        }
-        index += line.len() + 1;
+    for next in find_messages(input) {
+        messages.push(&input[start..next]);
+        start = next;
     }
 
-    None
+    messages.push(&input[start..]);
+
+    messages
 }
 
 #[cfg(test)]
@@ -68,15 +32,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn find_messages() {
+    fn capture_messages_test() {
         let input = include_str!("samples/multi_foo_messages.mbx");
+        let messages = capture_messages(input);
+        assert!(messages.len() == 4);
+    }
 
-        let first_message = find_next_message(input, 0);
-        assert!(first_message.is_some());
-        assert!(input[first_message.unwrap()..].starts_with("From barbarbar"));
-    
-        let second_message = find_next_message(input, first_message.unwrap());
-        assert!(second_message.is_some());
-        assert!(&input[second_message.unwrap()..].starts_with("From foobar"));
+    #[test]
+    fn find_messages_test() {
+        let input = include_str!("parser/mailbox/samples/multi_patches.mbx");
+        let messages = capture_messages(input);
+        dbg!(messages);
     }
 }
