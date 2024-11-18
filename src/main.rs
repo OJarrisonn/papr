0b7f7ecf1321bf7f574164b2d9b65bb59036ec0c
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-11-18 17:24:06 -0300

    refactor: color_eyre
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
 use std::{
-    error::Error,
     io::{stdin, Read},
     path::Path,
 };
 
 use clap::Parser;
 use cli::Args;
+use color_eyre::eyre::{Context, Result};
+use papr::parser::mailbox::Mailbox;
 
 pub mod cli;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<()> {
     let Args { files } = Args::parse();
 
     // If no files are provided, read from STDIN
     let files = if files.is_empty() {
         let mut content = String::new();
 
-        stdin().read_to_string(&mut content)?;
+        stdin().read_to_string(&mut content).with_context(|| "Failed to read input from STDIN")?;
 
         vec![("STDIN".to_string(), content)]
     } else {
-        read_all(files)?
+        read_all(files)
+            .with_context(|| "While opening files")?
     };
 
     for (path, content) in files {
-        let mailbox = papr::parser::mailbox::Mailbox::try_from(content.as_str())?;
+        let mailbox = Mailbox::try_from(content.as_str())?;
         dbg!(&mailbox);
         println!("{}:\n{}", path, mailbox);
     }
@@ -32,12 +34,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn read_all(files: Vec<String>) -> Result<Vec<(String, String)>, std::io::Error> {
+fn read_all(files: Vec<String>) -> Result<Vec<(String, String)>> {
     files
         .into_iter()
         .map(|file| {
             let path = Path::new(&file);
-            let content = std::fs::read_to_string(path)?;
+            let content = std::fs::read_to_string(path)
+                .with_context(|| format!("File {} could not be opened", &path.display()))?;
 
             Ok((file, content))
         })
