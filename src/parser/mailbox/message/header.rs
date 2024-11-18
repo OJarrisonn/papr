438d47decc70850304d07c110571859ef3ba9e97
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-11-17 21:57:17 -0300

    feat: parse person header
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- src/parser/mailbox/message/header.rs
+++ src/parser/mailbox/message/header.rs
@@ -1,32 +1,19 @@
+use std::fmt::Display;
+
 use chrono::{DateTime, Utc};
+use regex::Regex;
+
+const PERSON_REGEX: &str = r"(?P<name>\w+(\s\w+)*)\s*<(?P<email>[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)>";
+const PERSON_NO_NAME_REGEX: &str = r"<(?P<email>[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)>";
+const PERSON_AT_REGEX: &str = r#"(?P<name>\w+(\s\w+)*)\s*at\s*<"(?P<email>[a-zA-Z0-9_.+-]+ at [a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)">"#;
+const PERSON_AT_NO_NAME_REGEX: &str = r#""(?P<email>[a-zA-Z0-9_.+-]+ at [a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)""#;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Header<'input> {
-    From(&'input str),
+    From(Person<'input>),
     Date(DateTime<Utc>),
-    Author(&'input str),
+    Author(Person<'input>),
     Subject(&'input str),
-    To(&'input str),
-    Cc(&'input str),
-    Bcc(&'input str),
-    MessageId(&'input str),
-    InReplyTo(&'input str),
-    References(&'input str),
-    ReplyTo(&'input str),
-    Sender(&'input str),
-    ReturnPath(&'input str),
-    Received(&'input str),
-    MimeVersion(&'input str),
-    ContentType(&'input str),
-    ContentTransferEncoding(&'input str),
-    ContentId(&'input str),
-    ContentDescription(&'input str),
-    ContentDisposition(&'input str),
-    ContentLanguage(&'input str),
-    ContentLocation(&'input str),
-    ContentMD5(&'input str),
-    ContentLength(&'input str),
-    Content(&'input str),
     Other(&'input str, &'input str),
 }
 
@@ -38,7 +25,7 @@ impl<'input> TryFrom<(&'input str, &'input str)> for Header<'input> {
         let lkey = key.to_lowercase();
 
         match lkey.as_str() {
-            "from" => Ok(Header::From(value)),
+            "from" => Ok(Header::From(value.try_into()?)),
             "date" => Ok(
                 Header::Date(
                     DateTime::parse_from_rfc2822(value)
@@ -46,41 +33,120 @@ impl<'input> TryFrom<(&'input str, &'input str)> for Header<'input> {
                         .map(|dt| dt.to_utc())?
                 )
             ),
+            "author" => Ok(Header::Author(value.try_into()?)),
             "subject" => Ok(Header::Subject(value)),
-            "to" => Ok(Header::To(value)),
-            "cc" => Ok(Header::Cc(value)),
-            "bcc" => Ok(Header::Bcc(value)),
-            "message-id" => Ok(Header::MessageId(value)),
-            "in-reply-to" => Ok(Header::InReplyTo(value)),
-            "references" => Ok(Header::References(value)),
-            "reply-to" => Ok(Header::ReplyTo(value)),
-            "sender" => Ok(Header::Sender(value)),
-            "return-path" => Ok(Header::ReturnPath(value)),
-            "received" => Ok(Header::Received(value)),
-            "mime-version" => Ok(Header::MimeVersion(value)),
-            "content-type" => Ok(Header::ContentType(value)),
-            "content-transfer-encoding" => Ok(Header::ContentTransferEncoding(value)),
-            "content-id" => Ok(Header::ContentId(value)),
-            "content-description" => Ok(Header::ContentDescription(value)),
-            "content-disposition" => Ok(Header::ContentDisposition(value)),
-            "content-language" => Ok(Header::ContentLanguage(value)),
-            "content-location" => Ok(Header::ContentLocation(value)),
-            "content-md5" => Ok(Header::ContentMD5(value)),
-            "content-length" => Ok(Header::ContentLength(value)),
-            "content" => Ok(Header::Content(value)),
             _ => Ok(Header::Other(key, value)),
         }
     }
 }
+
+impl Display for Header<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Header::From(person) => write!(f, "From: {}", person),
+            Header::Date(date) => write!(f, "Date: {}", date.to_rfc2822()),
+            Header::Author(person) => write!(f, "Author: {}", person),
+            Header::Subject(subject) => write!(f, "Subject: {}", subject),
+            Header::Other(key, value) => write!(f, "{}: {}", key, value),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Person<'input> {
+    pub name: Option<&'input str>,
+    pub email: &'input str,
+}
+
+impl<'input> TryFrom<&'input str> for Person<'input> {
+    type Error = String;
+
+    fn try_from(value: &'input str) -> Result<Self, Self::Error> {
+        let re_person = Regex::new(PERSON_REGEX).unwrap();
+        let re_person_no_name = Regex::new(PERSON_NO_NAME_REGEX).unwrap();
+        let re_person_at = Regex::new(PERSON_AT_REGEX).unwrap();
+        let re_person_at_no_name = Regex::new(PERSON_AT_NO_NAME_REGEX).unwrap();
+
+        let captures = re_person.captures(value)
+            .or_else(|| re_person_no_name.captures(value))
+            .or_else(|| re_person_at.captures(value))
+            .or_else(|| re_person_at_no_name.captures(value))
+            .ok_or("Invalid person")?;
+
+        let name = captures.name("name").map(|name| name.as_str());
+        let email = captures.name("email").ok_or("Invalid email")?.as_str();
+
+        Ok(Person { name, email })
+    }
+}
+
+impl Display for Person<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(name) = self.name {
+            write!(f, "{} <{}>", name, self.email)
+        } else {
+            write!(f, "<{}>", self.email)
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Email<'input> {
+    pub user: &'input str,
+    pub domain: &'input str,
+}
+
+impl<'input> TryFrom<&'input str> for Email<'input> {
+    type Error = String;
+
+    fn try_from(value: &'input str) -> Result<Self, Self::Error> {
+        let parts: Vec<&str> = if value.contains('@') {
+            value.split('@').collect()
+        } else {
+            value.split(" at ").collect()
+        };
+
+        if parts.len() != 2 {
+            return Err(format!("Invalid email {}", value));
+        }
+
+        Ok(Email { user: parts[0], domain: parts[1] })
+    }
+}
+
+impl Display for Email<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}@{}", self.user, self.domain)
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn parse_header() {
-        let header = Header::try_from(("Date", "Wed, 08 Jun 2022 12:00:01 -0300"));
+    fn parse_person_header() {
+        let header = Header::try_from(("From", "Foo Bar <foo@baz.com>"));
         dbg!(&header);
+    }
+
+    #[test]
+    fn parse_date_header() {
+        let header = Header::try_from(("Date", "Wed, 08 Jun 2022 12:00:01 -0300"));
         assert!(header.is_ok());
+
+        let header = header.unwrap();
+        assert_eq!(header, Header::Date(DateTime::parse_from_rfc2822("Wed, 08 Jun 2022 12:00:01 -0300").unwrap().to_utc()));
+    }
+
+    #[test]
+    fn parse_person() {
+        let person = Person::try_from("Foo Bar <foo.bar@bar.com>");
+        assert!(person.is_ok());
+
+        let person = person.unwrap();
+        assert_eq!(person.name, Some("Foo Bar"));
+        assert_eq!(person.email, "foo.bar@bar.com");
     }
 }
