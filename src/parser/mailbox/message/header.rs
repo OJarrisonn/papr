376ed8af250af6fa28ef3beea1ff55a779c12b90
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-11-17 22:27:29 -0300

    feat: subjects parse and format
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- src/parser/mailbox/message/header.rs
+++ src/parser/mailbox/message/header.rs
@@ -8,6 +8,10 @@ const PERSON_NO_NAME_REGEX: &str = r"<(?P<email>[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.
 const PERSON_AT_REGEX: &str = r#"(?P<name>\w+(\s\w+)*)\s*at\s*<"(?P<email>[a-zA-Z0-9_.+-]+ at [a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)">"#;
 const PERSON_AT_NO_NAME_REGEX: &str = r#""(?P<email>[a-zA-Z0-9_.+-]+ at [a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)""#;
 
+const PATCH_SUBJECT_REGEX: &str = r"^\[PATCH( v(?P<version>\d+))? (?P<index>\d+)/(?P<total>\d+)\] (?P<tags>([^:]+:)*)(?P<description>.+)$";
+const TAGGED_SUBJECT_REGEX: &str = r"^(?P<tags>([^:]+:)+)(?P<description>.+)$";
+const SIMPLE_SUBJECT_REGEX: &str = r"^(?P<description>.+)$";
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Header<'input> {
     From(Person<'input>),
@@ -120,6 +124,71 @@ impl Display for Email<'_> {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Subject<'input> {
+    Simple(&'input str),
+    Tagged {
+        tags: Vec<&'input str>,
+        description: &'input str,
+    },
+    Patch {
+        version: Option<usize>,
+        index: Option<(usize, usize)>,
+        tags: Vec<&'input str>,
+        description: &'input str,
+    }
+}
+
+impl<'input> TryFrom<&'input str> for Subject<'input> {
+    type Error = String;
+
+    fn try_from(value: &'input str) -> Result<Self, Self::Error> {
+        let re_patch = Regex::new(PATCH_SUBJECT_REGEX).unwrap();
+        let re_tagged = Regex::new(TAGGED_SUBJECT_REGEX).unwrap();
+        let re_simple = Regex::new(SIMPLE_SUBJECT_REGEX).unwrap();
+
+        if let Some(captures) = re_patch.captures(value) {
+            let version = captures.name("version").map(|v| v.as_str().parse::<usize>().ok()).flatten();
+            let base = captures.name("index").map(|t| t.as_str().parse::<usize>().ok()).flatten();
+            let total = captures.name("total").map(|t| t.as_str().parse::<usize>().ok()).flatten();
+            let tags = captures.name("tags").ok_or("Invalid tags")?.as_str().split(':').map(|s| s.trim()).filter(|s| !s.is_empty()).collect();
+            let description = captures.name("description").map(|d| d.as_str()).map(|s| s.trim()).ok_or("Invalid description")?;
+
+            let index = match (base, total) {
+                (Some(base), Some(total)) => Some((base, total)),
+                _ => None,
+            };
+            Ok(Subject::Patch { version, index, tags, description })
+        } else if let Some(captures) = re_tagged.captures(value) {
+            let tags = captures.name("tags").ok_or("Invalid tags")?.as_str().split(':').map(|s| s.trim()).filter(|s| !s.is_empty()).collect();
+            let description = captures.name("description").map(|d| d.as_str()).map(|s| s.trim()).ok_or("Invalid description")?;
+            
+            Ok(Subject::Tagged { tags, description })
+        } else if let Some(captures) = re_simple.captures(value) {
+            let description = captures.name("description").map(|d| d.as_str()).map(|s| s.trim()).ok_or("Invalid description")?;
+
+            Ok(Subject::Simple(description))
+        } else {
+            Err("Invalid subject".to_string())
+        }
+    }
+}
+
+impl Display for Subject<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Subject::Simple(description) => write!(f, "{}", description),
+            Subject::Tagged { tags, description } => write!(f, "{}: {}", tags.join(": "), description),
+            Subject::Patch { version, index, tags, description } => {
+                let version = version.map(|v| format!(" v{}", v)).unwrap_or_default();
+                let index = index.map(|(i, t)| format!(" {}/{}", i, t)).unwrap_or_default();
+                let tags = if tags.len() > 0 { tags.join(": ") + ": " } else { "".to_string() };
+
+                write!(f, "[PATCH{}{}] {}{}", version, index, tags, description)
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -148,5 +217,41 @@ mod tests {
         let person = person.unwrap();
         assert_eq!(person.name, Some("Foo Bar"));
         assert_eq!(person.email, "foo.bar@bar.com");
+    }
+
+    #[test]
+    fn parse_subject() {
+        let subject = Subject::try_from("[PATCH v1 1/1] foo: bar: baz");
+        assert!(subject.is_ok());
+
+        let subject = subject.unwrap();
+        assert_eq!(subject, Subject::Patch { version: Some(1), index: Some((1, 1)), tags: vec!["foo", "bar"], description: "baz" });
+
+        let subject = Subject::try_from("[PATCH 0/2] some example patch");
+        assert!(subject.is_ok());
+
+        let subject = subject.unwrap();
+        assert_eq!(subject, Subject::Patch { version: None, index: Some((0, 2)), tags: vec![], description: "some example patch" });
+
+        let subject = Subject::try_from("foo: bar");
+        assert!(subject.is_ok());
+        
+        let subject = subject.unwrap();
+        assert_eq!(subject, Subject::Tagged { tags: vec!["foo"], description: "bar" });
+    }
+
+    #[test]
+    fn format_subject() {
+        let subject = Subject::Patch { version: Some(1), index: Some((1, 1)), tags: vec!["foo", "bar"], description: "baz" };
+        assert_eq!(subject.to_string(), "[PATCH v1 1/1] foo: bar: baz");
+
+        let subject = Subject::Patch { version: None, index: Some((0, 2)), tags: vec![], description: "some example patch" };
+        assert_eq!(subject.to_string(), "[PATCH 0/2] some example patch");
+
+        let subject = Subject::Tagged { tags: vec!["foo"], description: "bar" };
+        assert_eq!(subject.to_string(), "foo: bar");
+
+        let subject = Subject::Simple("baz foo barbar");
+        assert_eq!(subject.to_string(), "baz foo barbar");
     }
 }
